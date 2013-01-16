c878bba075bf502fd69e0051c7d3c2d12de5bb42
Tobias Haar <tobias.haar@googlemail.com>
2013-01-16 20:51:09 +0000

    creates a basic rails application as a start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TyneTicketsOrg::Application.config.secret_token = 'd5a7efd3bccd9441962c52c7952a266fe1affcc602cd377f6eb3680a3844c773ec2a6f7ee9d4beb0572453a9a9bdaa98e0e560c07bb5d1b4f55d7e6f947e99ac'
